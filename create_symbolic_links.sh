3ee192c466a7d8590ad4ca84f53309939af68832
Kevin Klinkel <kgklinkel@Kevins-MacBook-Pro.local>
2017-10-16 20:04:42 -0600

    Improved script

--- create_symbolic_links.sh
+++ create_symbolic_links.sh
@@ -1,42 +1,63 @@
+# This script creates symbolic links to all of the dotfiles in this repo.
+# The symbolic links will be located in the $HOME (~) directory.
+# 
+# The original files will be backed up, by changing their name to
+# $FILENAME-bak.
+# 
+# At the end, the script will remove these backup files for you, if you'd like.
+#-------------------------------------------------------------------------------
+
+
+#--------------------------------- VARIABLES -----------------------------------
 # get location of dotfiles repo
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 THIS_SCRIPT_NAME="$(basename "$0")"
 
-
+#--------------------------------- FUNCTIONS -----------------------------------
 create_backup() {
     filename="$1"
 
-    # create backup file
-    mv -v "$HOME/$filename" "$HOME/$filename-bak"
+    if [ -f "$HOME/$filename" ] || [ -d "$HOME/$filename" ]; then
+        # create backup file
+        mv -vi "$HOME/$filename" "$HOME/$filename-bak"
+    fi
 }
-
 
 create_link() {
     filename="$1"
 
-    # create symbolic link
+    # define symbolic link arguments
     TARGET="$DOTFILES_DIR/$filename"
     LINK_NAME="$HOME/$filename"
+
+    # create symbolic link
     ln -s $TARGET $LINK_NAME
 }
 
-
 remove_backups() {
     for filename in $files; do
-    if [ -n "$filename" ]; then
-        rm -i "${HOME}/${filename}-bak"
-    fi
+        backup_file="${HOME}/${filename}-bak"
+        if [ "$backup_file" != "$HOME" ]; then
+            if [ -f "$backup_file" ] || [ -d "$backup_file" ]; then
+                rm -i "$backup_file" 
+            fi
+        fi
     done
 }
 
 
-#-------------------------------------------------------------------------------
+#------------------------------------ MAIN -------------------------------------
+# cd to the dotfiles repo directory, so it only gets listings of files in repo
+cd $DOTFILES_DIR
+
+# get list of all files in dotfiles repo
 files=$(for file in $(ls -a); do echo $file ; done \
             | tail -n +3 \
             | grep -v README \
             | grep -v git \
             | grep -v "$THIS_SCRIPT_NAME" \
        )
+
 
 # create backups and symbolic links
 for file in $files; do
@@ -53,7 +74,7 @@ done
 # ask to remove backups
 echo     "Would you like to remove the backup files?"
 echo     "(will ask for each file to be removed)"
-echo -ne "  (Y/n) >  "
+echo -ne "  (y/n) >  "
 
 read response
 
