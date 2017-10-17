3089d01bef680fdfd9fec7fbcb10fb5068813fb8
Kevin Klinkel <kgklinkel@gmail.com>
2017-10-17 15:46:23 +0000

    Fixed setup script to run with sudo and check vim and tmux correctly

--- set_up_vim_and_tmux.sh
+++ set_up_vim_and_tmux.sh
@@ -2,21 +2,21 @@
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # update apt-get
-sudo apt-get update
+sudo apt update
 
 # install vim
-if [ -n "$(which vim)" ]; then
+if [ -z "$(which vim)" ]; then
     sudo apt install vim
 fi
 
 # install tmux
-if [ -n "$(which tmux)" ]; then
+if [ -z "$(which tmux)" ]; then
     sudo apt install tmux
 fi
 
 # install ctags
 sudo apt install exuberant-ctags
-ln -s /usr/bin/ctags /usr/local/bin/ctags
+sudo ln -s /usr/bin/ctags /usr/local/bin/ctags
 
 # install Vundle (for vim)
 git clone https://github.com/VundleVim/Vundle.vim.git $DOTFILES_DIR/.vim/bundle/Vundle.vim
