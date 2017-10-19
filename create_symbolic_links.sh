1d7c82a7c09546a84c272d1020ee07bc8314e135
Kevin Klinkel <kgklinkel@gmail.com>
2017-10-19 13:06:36 -0600

    changed create_symbolic_links script to prompt for backup action (d/k/r) git removed files in .vim/bundle/

--- create_symbolic_links.sh
+++ create_symbolic_links.sh
@@ -5,7 +5,6 @@
 # $FILENAME-bak.
 # 
 # At the end, the script will remove these backup files for you, if you'd like.
-#-------------------------------------------------------------------------------
 
 
 #--------------------------------- VARIABLES -----------------------------------
@@ -17,8 +16,7 @@ SETUP_VIM_AND_TMUX_SH="set_up_vim_and_tmux.sh"
 #--------------------------------- FUNCTIONS -----------------------------------
 create_backup() {
     filename="$1"
-
-    if [ -f "$HOME/$filename" ] || [ -d "$HOME/$filename" ]; then
+    if [ -e "$HOME/$filename" ]; then
         # create backup file
         mv -vi "$HOME/$filename" "$HOME/$filename-bak"
     fi
@@ -35,13 +33,36 @@ create_link() {
     ln -sv $TARGET $LINK_NAME
 }
 
-remove_backups() {
+remove_backups(){
     for filename in $files; do
         backup_file="${HOME}/${filename}-bak"
-        if [ "$backup_file" != "$HOME" ]; then
-            if [ -f "$backup_file" ] || [ -d "$backup_file" ]; then
-                rm -i "$backup_file" 
-            fi
+        if [ -e "$backup_file" ]; then
+            rm -v "$backup_file" 
+        fi
+    done
+}
+
+clean_up_backups() {
+    for filename in $files; do
+        backup_file="${HOME}/${filename}-bak"
+        if [ -e "$backup_file" ]; then
+
+            #rm -i "$backup_file" 
+
+            # give option to Delete, Keep, or Restore backup file
+            echo -ne "$backup_file  -->  Delete, Keep, or Restore?  (d/k/r) > "
+            read resp
+
+            case $resp in
+                d | D )     rm -v "$backup_file"
+                            ;;
+                k | K )     echo "($backup_file kept as backup)"
+                            ;;
+                r | R )     echo "Restoring backup file..."
+                            mv -v $backup_file ${HOME}/${filename}
+                            ;;
+                * )         echo "Invalid response! ($backup_file kept as backup)"
+            esac
         fi
     done
 }
@@ -65,12 +86,17 @@ files=$(for file in $(ls -a); do echo $file ; done \
             | grep -v "$SETUP_VIM_AND_TMUX_SH" \
        )
 
+
 #-------------------------------------------------------------------------------
 # display files that will be backed up and linked
-echo
+#---------------------------------------
+tput sgr0
 tput smul
+echo
 echo "files to be linked:"
 tput rmul
+#---------------------------------------
+
 for file in $files; do
     echo "  $file"
 done
@@ -86,12 +112,15 @@ fi
 
 #-------------------------------------------------------------------------------
 # create backups
+#---------------------------------------
 tput sgr0
-echo
 tput smul
+echo
 echo "Creating backup files"
 tput rmul
 tput setaf 4
+#---------------------------------------
+
 for file in $files; do
     if [ -n "$file" ]; then
         # create backup file
@@ -99,31 +128,52 @@ for file in $files; do
     fi
 done
 
+
 #-------------------------------------------------------------------------------
 # create symbolic links
+#---------------------------------------
 tput sgr0
-echo
 tput smul
+echo
 echo "Creating symbolic links"
 tput rmul
 tput setaf 5
+#---------------------------------------
+
 for file in $files; do
     if [ -n "$file" ]; then
         # create symbolic link
         create_link "$file"
     fi
 done
+
 tput sgr0
+
 
 #-------------------------------------------------------------------------------
 # ask to remove backups
+#---------------------------------------
 echo
-echo     "Would you like to remove the backup files?"
-echo     "(will ask for each file to be removed)"
-echo -ne "  (y/n) >  "
-
+tput setaf 1
+echo -ne "Would you like to remove the backup files (without confirmation)?  > "
+tput sgr0
 read response
 
-if [ "$response" = "y" ]; then
+#---------------------------------------
+if [ "$response" = "y"]; then
     remove_backups
+else
+    echo
+    echo "Would you like to clean up the backup files?"
+    echo "(you will be prompted to delete, keep, or restore each backup file)"
+    echo -ne "  (y/n) > "
+
+    read response
+    if [ "$response" = "y" ]; then
+        clean_up_backups
+    fi
 fi
+
+
+#-------------------------------------------------------------------------------
+echo "Finished."
